2a3827fa5bc208086e3757ad46b33a8e9218ce61
Joseph Ajireloja <40704218+josephajire@users.noreply.github.com>
2025-08-13 14:28:29 +0100

    New commit.

---
+++ python-assignment.py
@@ -0,0 +1,32 @@
+# My answers to PLP Week 2 Assignment for Python Programming
+
+# Empty list created
+my_list =[]
+
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+# Insert the value 15 at the second position in the list.
+my_list.insert(1, 15)
+
+
+# Extend my_list with another list: [50, 60, 70].
+another_list = [50, 60, 70]
+my_list.extend(another_list)
+
+
+# Remove the last element from my_list.
+del my_list[-1]
+
+
+# Sort my_list in ascending order.
+my_list.sort()
+
+
+# Find and print the index of the value 30 in my_list.
+print(my_list.index(15))
